9b9c564a9298ff5bbbef607b41ffac9b85914859
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-07-30 12:55:34 +0900

    initial commit

---
+++ polling-app-server/src/main/java/com/imamachi/twitterpolling/TwitterpollingApplication.java
@@ -0,0 +1,13 @@
+package com.imamachi.twitterpolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterpollingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterpollingApplication.class, args);
+    }
+
+}
---
+++ polling-app-server/src/main/java/com/imamachi/twitterpolling/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.imamachi.twitterpolling.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
