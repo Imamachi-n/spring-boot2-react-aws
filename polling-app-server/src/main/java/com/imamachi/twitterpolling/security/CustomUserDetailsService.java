cf7e3c59037c5941d3388f9c3a9ec02936cd4805
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-08-01 12:51:53 +0900

    prepare a JWT token provider

---
+++ polling-app-server/src/main/java/com/imamachi/twitterpolling/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.imamachi.twitterpolling.security;
+
+import com.imamachi.twitterpolling.model.User;
+import com.imamachi.twitterpolling.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail)
+        throws UsernameNotFoundException {
+
+        // Let people login with either username or email
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail)
+                );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        // エラー時は、独自のResourceNotFoundExceptionを呼び出し
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
---
+++ polling-app-server/src/main/java/com/imamachi/twitterpolling/security/UserPrincipal.java
@@ -0,0 +1,79 @@
+package com.imamachi.twitterpolling.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.imamachi.twitterpolling.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    @JsonIgnore
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal create(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
+                new SimpleGrantedAuthority(role.getName().name())
+        ).collect(Collectors.toList());
+
+        return new UserPrincipal(
+                user.getId(),
+                user.getName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // 同じオブジェクトの場合、True
+        if (this == o) return true;
+        // オブジェクトがNullもしくは異なるクラス名の場合、False
+        if (o == null || getClass() != o.getClass()) return false;
+        // IDが一致する場合、True
+        UserPrincipal that = (UserPrincipal) o;
+        return Objects.equals(id, that.id);
+    }
+}
